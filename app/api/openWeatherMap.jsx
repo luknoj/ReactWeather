afe3a035864590cca035a69e2799c27a234b1f3d
Titan <luknoj@gmail.com>
2017-11-13 21:43:11 +0100

    change to https

--- app/api/openWeatherMap.jsx
+++ app/api/openWeatherMap.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const OPEN_WEATHER_MAP_URL = 'http://api.openweathermap.org/data/2.5/weather?appid=d5d76d39df82a7a6da00f5e14cabe257&units=metric';
+const OPEN_WEATHER_MAP_URL = 'https://api.openweathermap.org/data/2.5/weather?appid=d5d76d39df82a7a6da00f5e14cabe257&units=metric';
  
 module.exports = {
     getTemp: function (location) {
