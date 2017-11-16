6bda3bd3bdff5cac248bcceaa18a7f3f01dee79e
Titan <luknoj@gmail.com>
2017-11-16 21:16:26 +0100

    added foundation and style the page, center things and add grid to it

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import {Link} from 'react-router';
 
 var Examples = () => {
     return(
         <div>
-            <h3>Examples</h3>
-            <p>Welcome to the examples</p>
+            <h1 className="text-center">Examples</h1>
+            <p>Here are few example locations to try out:</p>
+            <ol>
+                <li>
+                    <Link to='/?location=Piła'>Piła, PL</Link>
+                </li>
+                <li>
+                    <Link to='/?location=Poznań'>Poznań, PL</Link>
+                </li>
+            </ol>
         </div>
     );
 }
